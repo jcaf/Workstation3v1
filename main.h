15063dcfa2ae980d4b533957991011d9325d6806
jcaf <firwar21@gmail.com>
2021-05-12 17:08:37 -0500

    First commit

---
+++ main.h
@@ -0,0 +1,82 @@
+/*
+ * main.h
+ *
+ *  Created on: Apr 14, 2021
+ *      Author: jcaf
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+//BUZZER
+#define PORTWxBUZZER  	PORTD
+#define PORTRxBUZZER	PIND
+#define CONFIGIOxBUZZER DDRD
+#define PINxBUZZER 0
+
+//LED1
+#define PORTWxLED1  	PORTA
+#define PORTRxLED1		PINA
+#define CONFIGIOxLED1 	DDRA
+#define PINxLED1 0
+//LED2
+#define PORTWxLED2  	PORTB
+#define PORTRxLED2		PINB
+#define CONFIGIOxLED2 	DDRB
+#define PINxLED2 0
+
+//Outputs
+#define PORTWxOUT_Z PORTC
+#define PORTRxOUT_Z	PINC
+#define CONFIGIOxOUT_Z DDRC
+#define PINxOUT_Z 4
+
+#define PORTWxOUT_Y PORTC
+#define PORTRxOUT_Y	PINC
+#define CONFIGIOxOUT_Y DDRC
+#define PINxOUT_Y 5
+
+#define PORTWxOUT_X PORTC
+#define PORTRxOUT_X	PINC
+#define CONFIGIOxOUT_X DDRC
+#define PINxOUT_X 6
+
+#define PORTWxOUT_4 PORTC
+#define PORTRxOUT_4	PINC
+#define CONFIGIOxOUT_4 DDRC
+#define PINxOUT_4 0
+
+#define PORTWxOUT_3 PORTC
+#define PORTRxOUT_3	PINC
+#define CONFIGIOxOUT_3 DDRC
+#define PINxOUT_3 1
+
+#define PORTWxOUT_2 PORTC
+#define PORTRxOUT_2	PINC
+#define CONFIGIOxOUT_2 DDRC
+#define PINxOUT_2 2
+
+#define PORTWxOUT_1 PORTC
+#define PORTRxOUT_1	PINC
+#define CONFIGIOxOUT_1 DDRC
+#define PINxOUT_1 3
+
+#define PORTWxOUT_R PORTD
+#define PORTRxOUT_R PIND
+#define CONFIGIOxOUT_R DDRD
+#define PINxOUT_R 7
+
+#define PORTWxOUT_S PORTD
+#define PORTRxOUT_S	PIND
+#define CONFIGIOxOUT_S DDRD
+#define PINxOUT_S 6
+
+#define PORTWxOUT_T PORTD
+#define PORTRxOUT_T	PIND
+#define CONFIGIOxOUT_T DDRD
+#define PINxOUT_T 5
+
+
+
+
+#endif /* MAIN_H_ */
